fd934cbecbff865a5884b1f661c1be33ee0104d6
Daniel Kudwien <daniel@netzstrategen.com>
2016-02-11 15:05:43 +0000

    Added post title to position options.

--- templates/dropdown.php
+++ templates/dropdown.php
@@ -11,10 +11,10 @@ $found_position = array_search((int) $post->ID, $placements, TRUE);
 <div class="misc-pub-section category-add">
   <label for="placement"><?= __('Placement position', Plugin::L10N) ?></label>
   <select name="placement" id="placement">
-    <option value="-1"><?= __('- No position -', Plugin::L10N) ?></option>
+    <option value="-1"><?= __('– No position –', Plugin::L10N) ?></option>
     <?php for ($i = 0; $i < 10; $i++): ?>
       <option value="<?= $i ?>" <?= $found_position === $i ? 'selected' : '' ?>>
-        <?= sprintf(__('Position %d', Plugin::L10N), $i + 1) ?>
+        <?= sprintf(__('%d: %s', Plugin::L10N), $i + 1, $placements[$i] ? wp_trim_words(get_the_title($placements[$i]), 4) : '–') ?>
       </option>
     <?php endfor ?>
   </select>
